7818a36ad6a9017af30c1a99f7d9bc804cb6a031
Liam <liam@scalzulli.com>
2021-04-25 16:06:58 -0400

    bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="paragon",
-    version="0.0.4",
+    version="0.0.5",
     author="Liam Scalzulli",
     author_email="liamscalzulli@gmail.com",
     description=("A tiny command line benchmarking utility"),
